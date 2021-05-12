662b112f35dfcdd6ea59371753b152518c9029ac
luisfer <suarez412@gmail.com>
2021-05-12 15:34:28 -0500

    se creo la vista y el modelo

---
+++ CursoMVC/Models/User/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.User
+{
+    public class RegisterViewModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PasswordConfirm { get; set; }
+    }
+}
---
+++ CursoMVC/Models/User/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoMVC.Models.User
+{
+    public class UserModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Cedula { get; set; }
+        public string Email { get; set; }
+        public string Perfil { get; set; }
+        public string Password { get; set; }
+        
+    }
+}
