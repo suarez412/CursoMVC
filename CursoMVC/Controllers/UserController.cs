1de2e9aea79e5ad6927b14be13be7ddc5956a5e7
luisfer <suarez412@gmail.com>
2021-05-26 15:45:01 -0500

    se agrego vistas editar eliminar

--- CursoMVC/Controllers/UserController.cs
+++ CursoMVC/Controllers/UserController.cs
@@ -3,15 +3,81 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursoMVC.Models.User;
 
 namespace CursoMVC.Controllers
 {
     public class UserController : Controller
     {
+        private UserDBContext db = new UserDBContext();
         // GET: User
         public ActionResult RegisterUser()
         {
             return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult RegisterUser(UserModel model)
+        {
+            try
+            {
+                model.Perfil = "Nivel_1";
+                db.Users.Add(model);
+                db.SaveChanges();
+                return RedirectToAction("ListUsers");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult ListUsers()
+        {
+            var Users = from e in db.Users
+                        orderby e.Cedula
+                        select e;
+            return View(Users);
+        }
+
+        public ActionResult EditUser(string id)
+        {
+            var User = db.Users.Single(x => x.Cedula == id);
+            return View(User);
+        }
+
+        [HttpPost]
+        public ActionResult EditUser(string cedula,FormCollection collection)
+        {
+            var User = db.Users.Single(x => x.Cedula == cedula);
+            if(TryUpdateModel(User))
+            {
+                db.SaveChanges();
+                return RedirectToAction("ListUsers");
+            }
+            return View(User);
+        }
+
+        public ActionResult DeleteUser(string id)
+        {
+            var User = db.Users.Single(x => x.Cedula == id);            
+            return View(User);
+        }
+
+        [HttpPost]
+        public ActionResult DeleteUser(string id, FormCollection collection)
+        {
+            var User = db.Users.Single(x => x.Cedula == id);
+            try
+            {
+                db.Users.Remove(User);
+                db.SaveChanges();
+                return RedirectToAction("ListUsers");
+            }
+            catch (Exception e)
+            {
+                return View();
+            }           
         }
     }
 }
--- CursoMVC/Models/User/UserModel.cs
+++ CursoMVC/Models/User/UserModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -10,10 +11,17 @@ namespace CursoMVC.Models.User
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
+        [Key]
         public string Cedula { get; set; }
         public string Email { get; set; }
         public string Perfil { get; set; }
         public string Password { get; set; }
         
+    }
+
+    public class UserDBContext : DbContext
+    {
+        public UserDBContext() { }
+        public DbSet<UserModel> Users { get; set; }
     }
 }
